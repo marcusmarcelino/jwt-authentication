d5695046fe2d69e4478eeb4cf07156b22ea27979
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2023-06-13 10:29:15 -0300

    ajustes na implementação

--- src/main/java/br/com/springboot/modules/user/services/CreateUserService.java
+++ src/main/java/br/com/springboot/modules/user/services/CreateUserService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.springboot.modules.user.dto.AssignRoleToUserDTO;
